b8c6ca1e5d3b4e6c9e7b71c660751925dde9db8a
Jesus Zuñiga Trejo <zutjmx@gmail.com>
2026-02-26 12:41:09 -0600

    Implement product listing functionality by creating ProductoService and updating ProductoApp component

--- src/components/ProductoApp.jsx
+++ src/components/ProductoApp.jsx
@@ -1,31 +1,21 @@
-import { useState } from "react";
-
-const initProductos = [
-    { id: 1, nombre: 'Producto 1', precio: 10.99 },
-    { id: 2, nombre: 'Producto 2', precio: 19.99 },
-    { id: 3, nombre: 'Producto 3', precio: 5.49 },
-    { id: 4, nombre: 'Maple Bacon Jerky', precio: 6.99 },
-    { id: 5, nombre: 'Compact Hair Dryer', precio: 29.99 },
-    { id: 6, nombre: 'Pet First Aid Kit', precio: 29.99 },
-    { id: 7, nombre: 'Gardening Gloves with Claws', precio: 24.99 },
-    { id: 8, nombre: 'Smartphone Projector Kit', precio: 24.99 },
-    { id: 9, nombre: 'Mini Food Processor', precio: 39.99 },
-    { id: 10, nombre: 'Portable Pet Water Bottle', precio: 18.99 },
-    { id: 11, nombre: 'Lemon Zest Olive Oil', precio: 8.99 },
-    { id: 12, nombre: 'Electric Stir Fry Pan', precio: 49.99 },
-    { id: 13, nombre: 'Reusable Produce Bags Set', precio: 14.99 },
-]
+import { useEffect, useState } from "react";
+import { listProductos } from '../services/ProductoService';
 
 export const ProductoApp = () => {
 
-    const [ productos, setProductos ] = useState(initProductos)
+    const [ productos, setProductos ] = useState([]);
+
+    useEffect(() => {
+        setProductos(listProductos());
+    }, []);
 
     return (
         <>
             <div className="container mt-4 mb-4">
                 <div className="card">
                     <div className="card-body">
-                        <h1 className="card-title">Productos</h1>            
+                        <h2 className="card-title">Productos</h2>
+                        <h4 className="card-subtitle mb-2 text-body-secondary">Lista de productos disponibles</h4>            
                         <table className="table table-striped">
                             <thead>
                                 <tr>
@@ -36,11 +26,11 @@ export const ProductoApp = () => {
                             </thead>
                             <tbody>
                                 {productos.map(producto => (
-                                    <tr key={producto.id}>
-                                        <td>{producto.id}</td>
-                                        <td>{producto.nombre}</td>
-                                        <td>${producto.precio}</td>
-                                    </tr>
+                                        <tr key={producto.id}>
+                                            <td>{producto.id}</td>
+                                            <td>{producto.nombre}</td>
+                                            <td>${producto.precio}</td>
+                                        </tr>
                                     )
                                 )
                                 }
